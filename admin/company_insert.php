c1d296b93bd395163fb71e1b31ab43055493399d
Feroskhan <feros@cnglobal.com.au>
2018-12-18 17:51:36 +0530

    Update2

--- admin/company_insert.php
+++ admin/company_insert.php
@@ -8,14 +8,13 @@ global $pdo, $msg;
 function company_exist($phoneNo)
 {
     global $msg, $pdo;
-    $sql_select = "SELECT *
-        FROM company
-        WHERE CompanyName = " . $pdo->quote($phoneNo) . "
+    $sql_select = "SELECT * FROM company 
+    	WHERE PhoneNo = " . $pdo->quote($phoneNo) . "
         LIMIT 1";
     $stmt       = $pdo->query($sql_select);
     if ($stmt === false)
     {
-        $msg = 'Error querying Users. ';
+        $msg = 'Error querying Company. ';
         echo $msg;
         return NULL;
     }
@@ -32,21 +31,47 @@ function company_exist($phoneNo)
 
 function create_company()
 {
-    $un       = $_POST['username'];
-    $username = validate_empty($un, 'User Name');
-    if ($username !== false)
+    $cn       = $_POST['CompanyName'];
+    $companyname = validate_empty($cn, 'Company Name');
+    if ($companyname !== false)
     {
-        $tpNo      = $_POST['phoneno'];
+        $tpNo      = $_POST['PhoneNo'];
         $phoneNo   = validate_empty($tpNo, 'Phone No');
-        $userLevel = validate_selection($_POST['userlevel'], 'User Level');
+        $agentId = validate_selection($_POST['AgentId'], 'Agent');
         
-        $password = generateNumericOTP(10); //md5(uniqid($phoneNo, true));
-        
+        //image section start
+	    $imagename         = $_FILES['image']['name'];
+	    $imagetemp         = $_FILES['image']['tmp_name'];
+	    $imagetype         = $_FILES['image']['type'];
+	    $imagesize         = $_FILES["image"]["size"];
+	    $imageext          = pathinfo($imagename, PATHINFO_EXTENSION);
+	    $newimagename      = $CompanyId . "." . $imageext;
+	    
+	    if (!file_exists('images/companyLogo')) //create directory if not exist
+	    {
+	    	mkdir('images/companyLogo', 0777, true);
+		}
+	    $imagepath         = "images/companyLogo/" . $newimagename;
+    
+    	if (empty($imagename))
+    {
+        $msg = "Please Select Image";
+        return false;
+    }
+    else if ($imagetype == "image/jpg" || $imagetype == 'image/jpeg' || $imagetype == 'image/png' || $imagetype == 'image/gif')
+    {
+        if (!file_exists($imagepath))
+        {
+            if ($imagesize < 5000000) //check file size 5MB
+            {
+            	}
+            	}
+            	}
         if ($phoneNo)
         {
             if (!company_exist($phoneNo))
             {
-                return Insert_Company($userLevel, $username, $password, $phoneNo);
+                return Insert_Company($agentId, $companyname, $phoneNo);
             }
         }
     }
@@ -56,19 +81,19 @@ function create_company()
 function edit_company()
 {
 	$user_id = $_POST['user_id'];
-    $un       = $_POST['username'];
-    $username = validate_empty($un, 'User Name');
-    if ($username !== false)
+    $cn       = $_POST['username'];
+    $companyname = validate_empty($cn, 'User Name');
+    if ($companyname !== false)
     {
         $tpNo      = $_POST['phoneno'];
         $phoneNo   = validate_empty($tpNo, 'Phone No');
-        $userLevel = validate_selection($_POST['userlevel'], 'User Level');
+        $agentId = validate_selection($_POST['userlevel'], 'User Level');
         
         if ($phoneNo)
         {
         /*    if (!company_exist($phoneNo))
             {*/
-                return Update_User($user_id,$userLevel, $username, $phoneNo);
+                return Update_User($user_id,$agentId, $companyname, $phoneNo);
             /*}*/
         }
     }
@@ -81,125 +106,103 @@ function active_company()
 
 
 
-    if ($username !== false)
+    if ($companyname !== false)
     {
         $tpNo      = $_POST['phoneno'];
         $phoneNo   = validate_empty($tpNo, 'Phone No');
-        $userLevel = validate_selection($_POST['userlevel'], 'User Level');
+        $agentId = validate_selection($_POST['userlevel'], 'User Level');
         
         if ($phoneNo)
         {
         /*    if (!company_exist($phoneNo))
             {*/
-                return Update_User($user_id,$userLevel, $username, $phoneNo);
+                return Update_User($user_id,$agentId, $companyname, $phoneNo);
             /*}*/
         }
     }
     return false;
 }
 
-function Insert_Company($userLevel, $username, $password, $phoneNo)
+function Insert_Company($agentId, $companyname, $phoneNo)
 {
 	global $pdo, $msg;
 	$statement = $pdo->prepare("
-			INSERT INTO company(Id, CompanyId, AgentId, CompanyName, AddressLine1, AddressLine2, AddressLine3, ContactPerson, PhoneNo, Mobile, Email, Latitude, Longitude, Status, StatusUpdatedBy, StatusUpdatedDate, CreatedBy, CreatedDate) 
-			VALUES (:CompanyId, :AgentId, :CompanyName, :AddressLine1, :AddressLine2, :AddressLine3, :ContactPerson, :PhoneNo, :Mobile, :Email, :Latitude, :Longitude, :Status, :StatusUpdatedBy, :StatusUpdatedDate, :CreatedBy, :CreatedDate)
+			INSERT INTO company(Id, CompanyId, AgentId, CompanyName, AddressLine1, 
+			AddressLine2, AddressLine3, ContactPerson, PhoneNo, Mobile, Email, 
+			Latitude, Longitude, Status, StatusUpdatedBy, StatusUpdatedDate, 
+			CreatedBy, CreatedDate) 
+			VALUES (:CompanyId, :AgentId, :CompanyName, :AddressLine1, :AddressLine2, 
+			:AddressLine3, :ContactPerson, :PhoneNo, :Mobile, :Email, 
+			:Latitude, :Longitude, :Status, :StatusUpdatedBy, :StatusUpdatedDate, 
+			:CreatedBy, :CreatedDate)
 		");
 
-		$crypt_pass        = md5($password);
-	    $phrase = '';
-	    if ($userLevel === '1')
-	    {
-	        $phrase = 'MVSU';
-	    }
-	    else if ($userLevel === '2')
-	    {
-	        $phrase = 'MVSY';
-	    }
-	    else if ($userLevel === '3')
-	    {
-	        $phrase = 'MV1';
-	    }
-	    else if ($userLevel === '4')
-	    {
-	        $phrase = 'MV2';
-	    }
-	    else
-	    {
-	        $phrase = 'MVU';
-	    }
-    
 		$output='';
     	
             $output = $statement->execute(
 				array(
-					':UserId' =>	GenerateRandomId(5, $phrase),
-					':UserLevel' =>	$userLevel,
-					':UserName' =>	$username,
-					':Password' =>	$crypt_pass, 
-					':PhoneNo' =>	$phoneNo,
-					':Status' =>	1,
+					':CompanyId' =>	GenerateRandomId(5, 'MVC'),
+					':AgentId' => $agentId,
+					':CompanyName' => $companyname,
+					':AddressLine1' => $_POST['AddressLine1'],
+					':AddressLine2' => $_POST['AddressLine2'],
+					':AddressLine3' => $_POST['AddressLine3'],
+					':ContactPerson' => $_POST['ContactPerson'],
+					':PhoneNo' => $phoneNo,
+					':Mobile' => $_POST['Mobile'],
+					':Email' => $_POST['Email'],
+					':Latitude' => $_POST['Latitude'],
+					':Longitude' => $_POST['Longitude'],
+					':Status' =>	0,
 					':StatusUpdatedBy' =>	$_SESSION['UserId'],
 					':StatusUpdatedDate' =>	date('Y-m-d H:i:s'),
 					':CreatedBy' =>	$_SESSION['UserId'],
 					':CreatedDate'=>	date('Y-m-d H:i:s')
 				)
 			);
-
-		$message1   = 'Congratulations! Your Phone has successfully registerd with www.myvehicle.lk. ';
-    	$message2   = 'UserName:' . $username . ' Password:' . $password.'. ';
     	
 		if ($output === false)
 	    {
-	        $msg = 'Error creating the user.';
+	        $msg = 'Error creating the Company.';
 	        return false;
 	        echo $msg;
 	    }
 	    else
 	    {
-	    	
-	        $msg = "The new user $phoneNo is created. ";
-	        SMS($message1, $phoneNo);
-	        SMS($message2, $phoneNo);
+	        $msg = "The new Company $companyname is created. ";
 	        echo $msg;
 	        return true;
 	    }
 	    
 }
 
-function Update_User($user_id, $userLevel, $username, $phoneNo)
+function Update_User($user_id, $agentId, $companyname, $phoneNo)
 {
 	global $pdo, $msg;
 	
 	$statement = $pdo->prepare(		
-			"UPDATE company SET CompanyId=:CompanyId,AgentId=:AgentId,CompanyName=:CompanyName,AddressLine1=:AddressLine1,AddressLine2=:AddressLine2,AddressLine3=:AddressLine3,ContactPerson=:ContactPerson,PhoneNo=:PhoneNo,Mobile=:Mobile,Email=Email,Latitude=:Latitude,Longitude=:Longitude,Status=:Status,StatusUpdatedBy=:StatusUpdatedBy,StatusUpdatedDate=:StatusUpdatedDate,CreatedBy=:CreatedBy,CreatedDate=:CreatedDate WHERE Id=:id"			
+			"UPDATE company SET CompanyName=:CompanyName,AddressLine1=:AddressLine1,
+			AddressLine2=:AddressLine2,AddressLine3=:AddressLine3,
+			ContactPerson=:ContactPerson,PhoneNo=:PhoneNo,Mobile=:Mobile,Email=Email,
+			Latitude=:Latitude,Longitude=:Longitude,StatusUpdatedBy=:StatusUpdatedBy,
+			StatusUpdatedDate=:StatusUpdatedDate 
+			WHERE Id=:id"			
 		);
 		
 		$result = $statement->execute(
 			array(
-			 ':CompanyId'	=>	,
-			 ':AgentId'	=>	,
-			 ':CompanyName'	=>	,
-			 ':AddressLine1'	=>	,
-			 ':AddressLine2'	=>	,
-			 ':AddressLine3'	=>	,
-			 ':ContactPerson'	=>	,
-			 ':PhoneNo'	=>	,
-			 ':Mobile'	=>	,
-			 ':Email'	=>	,
-			 ':Latitude'	=>	,
-			 ':Longitude'	=>	,
-			 ':Status'	=>	,
-			 ':StatusUpdatedBy'	=>	,
-			 ':StatusUpdatedDate'	=>	,
-			 ':CreatedBy'	=>	,
-			 ':CreatedDate'	=>	,
- 
-				':UserLevel'	=>	$userLevel,
-				':UserName'	=>	$username,
-				':PhoneNo'		=>	$phoneNo,
-				':StatusUpdatedBy'		=>	$_SESSION['UserId'],
-				':StatusUpdatedDate'		=>	date('Y-m-d H:i:s'),
+				':CompanyName' => $companyname,
+				':AddressLine1' => $_POST['AddressLine1'],
+				':AddressLine2' => $_POST['AddressLine2'],
+				':AddressLine3' => $_POST['AddressLine3'],
+				':ContactPerson' => $_POST['ContactPerson'],
+				':PhoneNo' => $phoneNo,
+				':Mobile' => $_POST['Mobile'],
+				':Email' => $_POST['Email'],
+				':Latitude' => $_POST['Latitude'],
+				':Longitude' => $_POST['Longitude'],
+				':StatusUpdatedBy' =>	$_SESSION['UserId'],
+				':StatusUpdatedDate' =>	date('Y-m-d H:i:s'),
 				':id'			=>	$user_id
 			)
 		);
